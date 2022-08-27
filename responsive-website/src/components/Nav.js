57d696d84220e7410ec8eba0e0658ea007244457
dy1g18 <denizz9@live.co.uk>
2022-08-27 16:20:49 +0100

    add css for Nav

--- responsive-website/src/components/Nav.js
+++ responsive-website/src/components/Nav.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import './Nav.css';
 
 const Nav = () =>{
     const [click, setClick] = useState(false);
@@ -9,28 +10,33 @@ const Nav = () =>{
 
     return(
         <div>
-            <nav className='nav'>
-                <div>
+            <nav className='navbar'>
+                <div className='navbar-container'>
                     <Link to='/' className='navbar-logo'>
-                        Brev <i className='' />
+                        Brev <i className='fab fa-typo3' />
                     </Link>
                     <div className='menu-icon' onClick={handleClick} >
-                        menu <i className={click ? '' : ''}/>
+                        menu <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
                     </div>
-                    <ul className={click ? 'menu-active' : 'nav-menu'}>
-                        <li>
-                            <Link to='/' className='' onClick={closeMenu}>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMenu}>
                                 Home
                             </Link>
                         </li>
-                        <li>
-                            <Link to='/idk' className='' onClick={closeMenu}>
+                        <li className='nav-item'>
+                            <Link to='/idk' className='nav-links' onClick={closeMenu}>
                                 idk
                             </Link>
                         </li>
-                        <li>
-                            <Link to='/idk2' className='' onClick={closeMenu}>
+                        <li className='nav-item'>
+                            <Link to='/idk2' className='nav-links' onClick={closeMenu}>
                                 idk2
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/idk2' className='nav-links-mobile' onClick={closeMenu}>
+                                sign up
                             </Link>
                         </li>
                     </ul>
