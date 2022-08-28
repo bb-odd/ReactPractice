51fc7c2087eef9c62534c0f258edc27b18ecf03d
dy1g18 <denizz9@live.co.uk>
2022-08-28 18:08:48 +0100

    init notes app

---
+++ notes/src/components/Note.js
@@ -0,0 +1,13 @@
+import React from './react';
+
+const Note = () =>{
+
+    return(
+        <div className='note'>
+            <h1></h1>
+            <a></a>
+        </div>
+    );
+}
+
+export default Note;
